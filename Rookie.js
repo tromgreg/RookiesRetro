e13b9ab18b3dbca0573293b062e6a82ea567e1b1
tromgreg <grelarson1@gmail.com>
2020-05-02 14:13:06 -0700

    update

--- Rookie.js
+++ Rookie.js
@@ -1,82 +1,50 @@
-$('#date-today h6').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, H:mm:ss a'));
-
-let workDay = {
-    "8 AM": "",
-    "9 AM": "",
-    "10 AM": "",
-    "11 AM": "",
-    "12 PM": "",
-    "1 PM": "",
-    "2 PM": "",
-    "3 PM": "",
-    "4 PM": "",
-    "5 PM": "",
-  };
-
-
-$(document).ready(function(){
-    if(!localStorage.getItem('workDay')){
-        updateCalendarTasks (workDay);
-    }else{
-        updateCalendarTasks (JSON.parse(localStorage.getItem('workDay')));
-    }
-})
-
-
-
-$(".nes-btn").click(function(){
-    console.log('click is registered');
-    var value = $(this).closest('.nes-container').find("textarea").val();
-    console.log('value: ',value);
-    var hourString = $(this).siblings("div").text();
-
-    saveSchedule(hourString, value);
-});
-
-function hourNumberFromHourString(hourString) {
-    switch(hourString) {
-        case "8 AM": return 8;
-        case "9 AM": return 9;
-        case "10 AM": return 10;
-        case "11 AM": return 11;
-        case "12 PM": return 12;
-        case "1 PM": return 13;
-        case "2 PM": return 14;
-        case "3 PM": return 15;
-        case "4 PM": return 16;
-        case "5 PM": return 17;
-    }
-  }
-
-  function loadCorrectDataSet(){
-    result =localStorage.getItem('workDay')
-    return (result ? result : workDay);
-
-  }
-  
-  function initializeLocalStorage() {
-      localStorage.setItem('workDay', JSON.stringify(workDay));
-  };
-
-function saveToLocalStorage(dayObject) {
-    localStorage.setItem('workDay', JSON.stringify(dayObject));
-}
-
-function saveSchedule(hourString, val){
-    if(!localStorage.getItem('workDay')){
-        initializeLocalStorage();
-    }
+$(document).ready(function () {
+        
+    var m = moment();
+    var currentTime = m.format('MMMM Do YYYY, h:mm:ss a');
+    var currentDate = m.format("MMM Do YYYY");
+    var currentHour = moment().hours();
+    console.log('current hour: ', currentHour)
     
-    let value = 
-    let workHours = JSON.parse(localStorage.getItem('workDay'))
-    workHours[hourString] = value
+    $("#currentDay").text(currentTime);
 
-    saveToLocalStorage(workHours);
-}
+    //cannot figure out i use (this)
+    $(".nes-btn").on("click", function () {
+        console.log("button");
+        var click = $(this).attr("data-value");
+        
+        var eventInput = $(click).val("nes-textarea");
+        console.log(eventInput);
+        localStorage.setItem(click, eventInput);
+    });
 
-function updateCalendarTasks(dayObject) {
-    $("#calendar-row").each(function(index) {
-        let res = $(this).closest('.nes-container');
-        $(this).find("textarea").text(dayObject[res.text()]);
-    })
-}    
+    let timeStorage = ["#8am", "#9am", "#10am", "#11pm", "#12pm", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm"];
+
+    for (let i = 0; i < timeStorage.length; i++){
+        let savedEvent = $('.nes-textarea')
+        console.log('localStorage loop');
+        $(timeStorage[i]).val(localStorage.getItem(timeStorage[i]));
+    };
+
+    //function timeStyle(){
+    //     for (let i = 6; i < 18; i++){
+             
+    //         let hour = '#' + i;
+    //         let scheduleHour = parseInt($(hour).attr("id"));
+    //         $(hour).removeClass();
+    //         if (
+    //             scheduleHour > currentHour) {
+    //             $(hour).attr("class", "row future");
+    //         }
+    //         else if (
+    //             scheduleHour === currentHour) {
+    //             $(hour).attr("class", "row present");
+    //         }
+    //         else if (
+    //             scheduleHour < currentHour) {
+    //             $(hour).attr("class", "row past");
+    //         }
+    //     };
+    // }
+    // timeStyle();
+});    
