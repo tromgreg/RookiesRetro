52b8905220e56a5f63568a15c66f63f62f964ebd
tromgreg <grelarson1@gmail.com>
2020-05-02 20:55:21 -0700

    finial push

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ Was able with a little help finally figure out where I went wrong with local sto
 
 5-1-20
 
+Took a bunch of stuff out trying to make the jqueary easier. $("textarea[data-value='" + click + "']") set a key and a value but since had gone over this JSON parse method before with brian I wasn't going to waste a bunch of time reinventing the wheel. especially because on the last homework I had such a time trying to use storage. 
+JSON Parse was not an easy task. As usual i felt like this whole thing is a frankenstein of stackoverflow(SOF). Even when I asked one of the TA's they sent me a SOF link.
+
+5-2-20
+found that by adding bootstrap i can be formatting with it and using NES CSS to keep the style true. I added the final touches AFTER I submitted. I got localStorage.getItem('workDay') and .setItem by JSON.stringify(workDay) and if statements to initialize the local stuff on refresh. I played around with this constantly and tried to use a different method but I guess the hard way is my right way. I didn't have enough time to get everything right. i'd like to have the fields clear on a new day and i'd like the css/bootstrap cleaned up.
+
+good riddens.
+
--- Rookie.js
+++ Rookie.js
@@ -1,52 +1,100 @@
-$(document).ready(function () {
-        
-    var m = moment();
-    var currentTime = m.format('MMMM Do YYYY, h:mm:ss a');
-    var currentDate = m.format("MMM Do YYYY");
-    var currentHour = moment().hours();
-    console.log('current hour: ', currentHour)
+//pulled from an earlier version that actually worked
+let workDay = {
+    "8 AM": "",
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+  };
+  
+  $(document).ready(function(){
+    if(!localStorage.getItem('workDay')) {
+      updateCalendarTasks(workDay);
+    } else {
+      updateCalendarTasks(JSON.parse(localStorage.getItem('workDay')));
+    }
+  })
+  
+  var m = moment();
+  var currentTime = m.format('MMMM Do YYYY, h:mm:ss a');
+  var currentDate = m.format("MMM Do YYYY");
+  var currentHour = moment().hours();
+  console.log('current hour: ', currentHour)
+  
+  $("#currentDay").text(currentTime);
+
+  let counter = 1;
+  for(const property in workDay) {
+    let textEntry = "#text-entry" + counter;
+    $(textEntry).text(workDay[property]);
+    let timeId = "#time" + counter;
+    let presentHour = moment().hour();
+    let timeString = $(timeId).text();
+    let timeNumber = hourNumberFromHourString(timeString);  
+    if(timeNumber < presentHour) {
+      $(textEntry).addClass("past-hour");
+    } else if (timeNumber > presentHour) {
+      $(textEntry).addClass("future-hour");
+    } else {
+      $(textEntry).addClass("present-hour");
+    }
+    counter ++;
+  }
+  
+  $("button").click(function() {
+    value = $(this).siblings("textarea").val();
+    hourString = $(this).siblings("div").text();
     
-    $("#currentDay").text(currentTime);
-
-    //cannot figure out i use (this)
-    $(".nes-btn").on("click", function () {
-        console.log("button");
-        var click = $(this).attr("data-value");
-        
-        var eventInput = $("textarea[data-value='" + click + "']");
-        var eventInputVal = eventInput.val();
-        localStorage.setItem(click, eventInputVal);
-    });
-    
-
-    let timeStorage = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17"];
-        console.log(timeStorage);
-        
-    for (let i = 0; i < timeStorage.length; i++){
-        let savedEvent = $('.nes-textarea')
-        console.log('localStorage loop');
-        $(timeStorage[i]).val(localStorage.getItem(timeStorage[i]));
-    };
-
-    //function timeStyle(){
-    //     for (let i = 6; i < 18; i++){
-             
-    //         let hour = '#' + i;
-    //         let scheduleHour = parseInt($(hour).attr("id"));
-    //         $(hour).removeClass();
-    //         if (
-    //             scheduleHour > currentHour) {
-    //             $(hour).attr("class", "row future");
-    //         }
-    //         else if (
-    //             scheduleHour === currentHour) {
-    //             $(hour).attr("class", "row present");
-    //         }
-    //         else if (
-    //             scheduleHour < currentHour) {
-    //             $(hour).attr("class", "row past");
-    //         }
-    //     };
-    // }
-    // timeStyle();
-});    
+    saveSchedule(hourString, value);
+  });
+  
+  function hourNumberFromHourString(hourString) {
+    switch(hourString) {
+      case "8 AM": return 8;
+      case "9 AM": return 9;
+      case "10 AM": return 10;
+      case "11 AM": return 11;
+      case "12 PM": return 12;
+      case "1 PM": return 13;
+      case "2 PM": return 14;
+      case "3 PM": return 15;
+      case "4 PM": return 16;
+      case "5 PM": return 17;
+    }
+  }
+  
+  function loadCorrectDataset() {
+    result = localStorage.getItem('workDay')
+    return (result ? result : workDay);
+  }
+  
+  function initializeLocalStorage() {
+    localStorage.setItem('workDay', JSON.stringify(workDay));
+  };
+  
+  function saveToLocalStorage(dayObj) {
+    localStorage.setItem('workDay', JSON.stringify(dayObj));
+  }
+  
+  function saveSchedule(hourString, val) {
+    if(!localStorage.getItem('workDay')) {
+      initializeLocalStorage();
+    }
+  
+    let workHours = JSON.parse(localStorage.getItem('workDay'));
+    workHours[hourString] = val
+  
+    saveToLocalStorage(workHours);
+  }
+  
+  function updateCalendarTasks(dayObject) {
+    $(".calendar-row").each(function(index) {
+      let res = $(this).children("div");
+      $(this).children("textarea").text(dayObject[res.text()]);
+    })
+  }
